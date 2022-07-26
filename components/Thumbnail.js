9a2e844291aa2b993b27f02a9645633919c7498c
Jake Baek <jakebaek@Jakes-MacBook-Air.local>
2022-07-25 20:53:36 -0400

    feat: added stylings on Results | added react-flip-move

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { ThumbUpIcon } from '@heroicons/react/outline';
+import { forwardRef } from "react";
 
-function Thumbnail({result}) {
+const Thumbnail = forwardRef(({result}, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
   return (
-    <div className="group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div className="group cursor-pointer transition duration-200 ease-in transform m-5 sm:hover:scale-105 hover:z-50">
       <Image 
         layout="responsive"
         src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`}
@@ -26,6 +27,6 @@ function Thumbnail({result}) {
       </div>
     </div>
   )
-}
+})
 
 export default Thumbnail;
