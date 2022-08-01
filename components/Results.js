65fb246ee691988ff229a48e10c2c598ff6fd80c
Jake Baek <jakebaek@Jakes-MacBook-Air.local>
2022-08-01 19:38:42 -0400

    fix: optimized

--- components/Results.js
+++ components/Results.js
@@ -1,13 +1,12 @@
 import Thumbnail from "./Thumbnail";
-import FlipMove from "react-flip-move";
 
 function Results({results}) {
   return (
-    <FlipMove className="px-5 my-12 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <ul className="px-5 my-12 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => {
         return <Thumbnail key={result.id} result={result}/>
       })}
-    </FlipMove>
+    </ul>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Results from '../components/Results'
 import requests from '../utils/requests';
 
 export default function Home({results}) {
-  console.log(results);
   return (
     <div>
       <Head>
@@ -25,7 +24,6 @@ export default function Home({results}) {
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
-  console.log(requests.fetchTrending.url);
   const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then((res) => res.json());
 
   return {
